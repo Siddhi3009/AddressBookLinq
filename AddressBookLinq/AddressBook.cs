415de40b4883edf7800d0e9802a9bb1849618284
Administrator <siddhiseth3009@gmail.com>
2020-11-08 17:15:06 +0530

    [Siddhi]Add. DataTable added

---
+++ AddressBookLinq/AddressBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLinq
+{
+    class AddressBook
+    {
+        DataTable dataTable = new DataTable();
+        /// <summary>
+        /// DataTable created for storing contact information
+        /// </summary>
+        public void CreateAddressBook()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("ZipCode", typeof(string));
+            dataTable.Columns.Add("PhoneNumber", typeof(string));
+            dataTable.Columns.Add("EmailID", typeof(string));
+        }
+    }
+}
