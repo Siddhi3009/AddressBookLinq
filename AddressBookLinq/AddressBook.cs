f3d69b9ab29eb072c6e2cd2a6e5a065d08824b6e
Administrator <siddhiseth3009@gmail.com>
2020-11-09 13:22:09 +0530

    [Siddhi]Add. EditContact method and contact class added

--- AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace AddressBookLinq
@@ -21,13 +22,7 @@ namespace AddressBookLinq
             dataTable.Columns.Add("ZipCode", typeof(string));
             dataTable.Columns.Add("PhoneNumber", typeof(string));
             dataTable.Columns.Add("EmailID", typeof(string));
-        }
-        /// <summary>
-        /// Insert Contacts in a the addressBook
-        /// </summary>
-        public void InsertContacts()
-        {
-            dataTable.Rows.Add("Harry", "Potter", "Station 9 4/3", "Hogwards", "London", "582465", "852963147", "harryporter@gmail.com");
+            //Some pre existing contacts added
             dataTable.Rows.Add("Sheldon", "Cooper", "Street 4", "Casper", "Texas", "525252", "9876778434", "cooper@yahoo.com");
             dataTable.Rows.Add("Siddhi", "Seth", "Gopal Vihar", "Jabalpur", "Madhya Pradesh", "856985", "7458658925", "siddhiseth@gmail.com");
             dataTable.Rows.Add("Priyanka", "Chopra", "Street 5", "NewYork", "NewYork", "520147", "5201118267", "priyanka@gmail.com");
@@ -38,6 +33,58 @@ namespace AddressBookLinq
             dataTable.Rows.Add("Captain", "Hook", "Sea Waters", "Island", "California", "452652", "6767986886", "hook@gmail.com");
             dataTable.Rows.Add("Monica", "Gellar", "Richwood", "Manhattan", "NewYork", "652369", "9874523555", "monicagellar@yahoo.com");
         }
-
+        /// <summary>
+        /// Insert Contacts in a the addressBook
+        /// </summary>
+        /// <param name="contact"></param>
+        public void InsertContacts(Contact contact)
+        {
+            dataTable.Rows.Add(contact.FirstName, contact.LastName, contact.Address, contact.City, contact.State, contact.ZipCode, contact.PhoneNumber, contact.Email);
+            Console.WriteLine("Contact inserted successfully");
+        }
+        /// <summary>
+        /// Display address book data table
+        /// </summary>
+        public void DisplayAddressBook()
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn col in dataTable.Columns)
+                {
+                    Console.Write(row[col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        /// <summary>
+        /// Edits the existing contact in datatable
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="address"></param>
+        /// <param name="city"></param>
+        /// <param name="state"></param>
+        /// <param name="zipcode"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="email"></param>
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipcode, string phoneNumber, string email)
+        {
+            var recordedData = dataTable.AsEnumerable().Where(x => x.Field<string>("FirstName") == firstName).FirstOrDefault();
+            if (recordedData != null)
+            {
+                recordedData.SetField("LastName", lastName);
+                recordedData.SetField("Address", address);
+                recordedData.SetField("City", city);
+                recordedData.SetField("State", state);
+                recordedData.SetField("ZipCode", zipcode);
+                recordedData.SetField("EmailID", email);
+                recordedData.SetField("State", state);
+                Console.WriteLine("Contact edited successfully");
+            }
+            else
+            {
+                Console.WriteLine("No Contact Found");
+            }
+        }
     }
 }
